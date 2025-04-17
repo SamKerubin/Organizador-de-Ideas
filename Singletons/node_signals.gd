7b03d0a4aef6ea42fb5dbf1e70f39f9c468bd8c4
SamKerubin <162046796+SamKerubin@users.noreply.github.com>
2025-04-17 15:11:33 -0500

    Revert "node bases... maybe?"

--- Singletons/node_signals.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-signal updated_relation(from_node: BaseNode, new_relation: BaseNode, to_node: BaseNode)
-signal node_created(new_node: BaseNode, relations: Dictionary[String, BaseNode])
-signal node_deleted(node_eliminated: BaseNode)
